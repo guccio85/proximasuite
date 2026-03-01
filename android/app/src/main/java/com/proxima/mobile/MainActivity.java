5d5c44ae6ae163f46d77ec88ef70d30212621ed1
guccio85 <sguccio@outlook.com>
2026-03-01 15:11:09 +0100

    fix: intercept intent:// URLs for AR Scene Viewer + bump versionName to 2.5.1

--- android/app/src/main/java/com/proxima/mobile/MainActivity.java
+++ android/app/src/main/java/com/proxima/mobile/MainActivity.java
@@ -1,5 +1,35 @@
 package com.proxima.mobile;
 
-import com.getcapacitor.BridgeActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Allow intent:// URLs so model-viewer AR (Scene Viewer) works inside WebView
+        bridge.getWebView().setWebViewClient(new BridgeWebViewClient(bridge) {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri uri = request.getUrl();
+                if (uri != null && "intent".equals(uri.getScheme())) {
+                    try {
+                        Intent intent = Intent.parseUri(uri.toString(), Intent.URI_INTENT_SCHEME);
+                        startActivity(intent);
+                        return true;
+                    } catch (ActivityNotFoundException | Exception e) {
+                        return false;
+                    }
+                }
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+    }
+}
